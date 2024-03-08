600b6bc1b7cfd479bd672002907803ef4b798f7c
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-08 15:50:33 +0300

    exercise 3

--- LAB-1-classes-and-objects-2.dart
+++ LAB-1-classes-and-objects-2.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 // class Rectangle {
 //   double width;
 //   double height;
@@ -21,21 +23,54 @@ void main() {
 
   // double perimeter = rectangle.calculatePerimeter();
   // print('Perimeter: $perimeter');
-  var product = Product('Smartphone', 500.0, 2);
+  // var product = Product('Smartphone', 500.0, 2);
 
-  // Calculating and displaying total cost
-  double totalCost = product.calculateTotalCost();
-  print('Total cost of ${product.name}: \$${totalCost.toStringAsFixed(2)}');
+  // // Calculating and displaying total cost
+  // double totalCost = product.calculateTotalCost();
+  // print('Total cost of ${product.name}: \$${totalCost.toStringAsFixed(2)}');
+  var circle = Circle(5.0);
+  // Calculating and displaying area of the circle
+  print('Area of Circle: ${circle.calculateArea().toStringAsFixed(2)}');
+
+  // Creating a Square object
+  var square = Square(4.0);
+  // Calculating and displaying area of the square
+  print('Area of Square: ${square.calculateArea().toStringAsFixed(2)}');
 }
 
-class Product {
-  String name;
-  double price;
-  int quantity;
+// class Product {
+//   String name;
+//   double price;
+//   int quantity;
 
-  Product(this.name, this.price, this.quantity);
+//   Product(this.name, this.price, this.quantity);
 
-  double calculateTotalCost() {
-    return price * quantity;
+//   double calculateTotalCost() {
+//     return price * quantity;
+//   }
+// }
+
+abstract class Shape {
+  double calculateArea();
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double calculateArea() {
+    return pi * radius * radius;
+  }
+}
+
+class Square extends Shape {
+  double side;
+
+  Square(this.side);
+  @override
+  double calculateArea() {
+    return side * side;
   }
 }
