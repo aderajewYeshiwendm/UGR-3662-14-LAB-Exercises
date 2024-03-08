59d4192f7405f66c529d18c359bb6d8c516d7212
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-08 15:12:20 +0300

    exercise 3

--- LAB-1-functions.dart
+++ LAB-1-functions.dart
@@ -1,24 +1,34 @@
 void main() {
   // var func = add(4, 4);
   // print(func);
-  checkPrime(5);
+  // checkPrime(5);
+  var a = reverse('root');
+  print(a);
 }
 
 // add(a, b) {
 //   return a + b;
 // }
 
-checkPrime(num) {
-  var count = 0;
-  for (int i = 1; i < num + 1; i++) {
-    if (num % i == 0) {
-      count += 1;
-    }
-  }
+// checkPrime(num) {
+//   var count = 0;
+//   for (int i = 1; i < num + 1; i++) {
+//     if (num % i == 0) {
+//       count += 1;
+//     }
+//   }
 
-  if (count > 2 || num <= 1) {
-    print('Not a prime');
-  } else {
-    print('Prime');
+//   if (count > 2 || num <= 1) {
+//     print('Not a prime');
+//   } else {
+//     print('Prime');
+//   }
+// }
+
+reverse(str) {
+  var reversedString = '';
+  for (int i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
   }
+  return reversedString;
 }
