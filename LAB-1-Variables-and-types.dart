31bcf654f53e3823b9535988469ac6a076f49594
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-08 14:37:00 +0300

    exercise2

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,6 +1,16 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+
 void main() {
   var name = 'Aderajew Yeshiwendm';
   var age = 21;
   var favoriteColor = 'Green';
   print('name: $name age: $age favoriteColor: $favoriteColor');
+
+  const int speed = 299792458;
+  print('enter distance:');
+  double distance = double.parse(stdin.readLineSync()!);
+  double second = distance / speed;
+  print('$second seconds');
 }
