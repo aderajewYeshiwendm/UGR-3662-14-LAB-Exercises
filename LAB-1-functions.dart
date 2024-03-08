9d4441cc14b6a2d11ac480ff65bee4e356bc1760
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-08 15:08:47 +0300

    exercise 3

--- LAB-1-functions.dart
+++ LAB-1-functions.dart
@@ -1,8 +1,24 @@
 void main() {
-  var func = add(4, 4);
-  print(func);
+  // var func = add(4, 4);
+  // print(func);
+  checkPrime(5);
 }
 
-add(a, b) {
-  return a + b;
+// add(a, b) {
+//   return a + b;
+// }
+
+checkPrime(num) {
+  var count = 0;
+  for (int i = 1; i < num + 1; i++) {
+    if (num % i == 0) {
+      count += 1;
+    }
+  }
+
+  if (count > 2 || num <= 1) {
+    print('Not a prime');
+  } else {
+    print('Prime');
+  }
 }
