917bbe61e1d784b97555ed969886b87f6f7be4e5
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-03-08 14:49:28 +0300

    exercise 3

--- LAB-1-control-flow.dart
+++ LAB-1-control-flow.dart
@@ -1,14 +1,31 @@
 import 'dart:io';
 
 void main() {
-  int number = int.parse(stdin.readLineSync()!);
-  if (number % 2 == 0) {
-    print('even');
-  } else {
-    print('odd');
-  }
+  // int number = int.parse(stdin.readLineSync()!);
+  // if (number % 2 == 0) {
+  //   print('even');
+  // } else {
+  //   print('odd');
+  // }
 
-  for (int i = 1; i <= 10; i++) {
-    print(i);
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  double a = double.parse(stdin.readLineSync()!);
+  double b = double.parse(stdin.readLineSync()!);
+  String? operation = stdin.readLineSync();
+  switch (operation) {
+    case '+':
+      print(a + b);
+      break;
+    case '-':
+      print(a - b);
+      break;
+    case '*':
+      print(a * b);
+      break;
+    default:
+      print(a / b);
   }
 }
