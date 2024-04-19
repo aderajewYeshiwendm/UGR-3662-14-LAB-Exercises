b2ef2507fe826055524518c10cdfb62d0f4a192c
Aderajew Yeshiwendm <aderajewy26@gmail.com>
2024-04-19 15:48:06 +0300

    state management

---
+++ state/lib/provider.dart
@@ -0,0 +1,93 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(
+//     ChangeNotifierProvider(
+//       create: (context) => Counter(),
+//       child: const MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: const Text('Flutter Demo Home Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Consumer<Counter>(
+//               builder: (context, counter, child) {
+//                 return Text(
+//                   '${counter.counter}',
+//                   style: Theme.of(context).textTheme.bodyMedium,
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () =>
+//                 Provider.of<Counter>(context, listen: false).incrementCounter(),
+//             tooltip: 'Increment',
+//             child: const Icon(Icons.add),
+//           ),
+//           FloatingActionButton(
+//             onPressed: () =>
+//                 Provider.of<Counter>(context, listen: false).decrementCounter(),
+//             tooltip: 'Decrement',
+//             child: const Icon(Icons.minimize),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class Counter extends ChangeNotifier {
+//   int _counter = 0;
+
+//   int get counter => _counter;
+
+//   void incrementCounter() {
+//     _counter++;
+//     notifyListeners();
+//   }
+
+//   void decrementCounter() {
+//     _counter--;
+//     notifyListeners();
+//   }
+// }
